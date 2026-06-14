e9b707a0b3959f8e501da872ecff551dac8be816
kiyu520 <2186925409@qq.com>
2026-06-14 22:57:36 +0800

    update args with commandLine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import argparse
 
 api_key = os.getenv("DEEPSEEK_API_KEY")
 
@@ -10,12 +11,15 @@ headers = {
     "Authorization": f"Bearer {api_key}"
 }
 
-msg=input()
+# msg=input()
+parser=argparse.ArgumentParser()
+parser.add_argument("msg",type=str,help="your question")
+args=parser.parse_args()
 payload = {
     "model": "deepseek-v4-flash",
     "messages": [
         {"role": "system", "content": "你是一个命令行AI助手。回答用户问题时必须：(1) 只给出确认的事实，绝不捏造信息；(2) 用最简洁的语言直接回答，通常不超过1-2句;(3) 不使用任何格式（如Markdown、列表、代码块）；(4) 如果不知道就说“不清楚”,避免任何解释或附加内容。"},
-        {"role": "user", "content": f"{msg}"}
+        {"role": "user", "content": f"{args.msg}"}
     ],
     "thinking": {"type": "enabled"},
     "reasoning_effort": "high",
